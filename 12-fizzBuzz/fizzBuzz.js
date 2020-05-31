2019cf6208b5e5f0b18ade57b6e4fc53a705034e
Michael Odusanya <modusanya12@gmail.com>
2020-05-31 15:20:03 -0400

    does what its supposed to do

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,6 +1,46 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+  let arr = [];
+  let inc = start;
+  while(inc < end)
+  {
+    if(inc % 3 == 0 && inc % 5 == 0)
+    {
+      arr.push("FizzBuzz");
+    }
+    else if(inc % 3 == 0)
+    {
+      arr.push("Fizz");
+    }
+    else if(inc % 5 == 0)
+    {
+      arr.push("Buzz");
+    }
+    else
+    {
+      arr.push(inc);
+    }
+    inc++;
+  }
+
+  if(end % 3 == 0 && end % 5 == 0)
+  {
+    arr.push("FizzBuzz");
+  }
+  else if(end % 3 == 0)
+  {
+    arr.push("Fizz");
+  }
+  else if(end % 5 == 0)
+  {
+    arr.push("Buzz");
+  }
+  else
+  {
+    arr.push(end);
+  }
+  return arr;
 }
+console.log(fizzBuzz(15,30));
 
 // Do not edit this line;
 module.exports = fizzBuzz;
