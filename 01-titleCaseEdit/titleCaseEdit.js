8f3757a505cc5ec085526d56575cce7e334a8d02
Michael Odusanya <modusanya12@gmail.com>
2020-05-31 13:49:58 -0400

    Fixed case edit function

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,17 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  let titleList = title.split(" ");
+  let result = "";
+  for(let i=0; i<titleList.length; i++)
+  {
+    let s = titleList[i].replace(titleList[i].charAt(0),titleList[i].charAt(0).toUpperCase());
+    result += s + " ";
+  }
+  return result;
 }
+let word = "orange juice is king";
+
+console.log(titleCaseEdit(word));
+
 
 // Do not edit this line;
 module.exports = titleCaseEdit;
