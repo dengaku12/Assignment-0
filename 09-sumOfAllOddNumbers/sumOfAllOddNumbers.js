0bd151f00f5ffb5708388e580a040c557fbd9618
Michael Odusanya <modusanya12@gmail.com>
2020-05-31 14:43:14 -0400

    counts odd numbers in array

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,6 +1,15 @@
 function sumOfAllOddNumbers(nums) {
-  // Insert code here;
+  let count =0;
+  for(let i=0; i<nums.length; i++)
+  {
+    if(nums[i] % 2 == 1)
+    {
+      count++;
+    }
+  }
+  return count;
 }
+console.log(sumOfAllOddNumbers([12,15,13,18]));
 
 // Do not edit this line;
 module.exports = sumOfAllOddNumbers;
