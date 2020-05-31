cf9d67524f2ac37143b7565862f192d4b26f6fa9
Michael Odusanya <modusanya12@gmail.com>
2020-05-31 14:32:44 -0400

    counts if index matches element

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,6 +1,16 @@
 function countOfAllIndexMatchingNumbers(nums) {
-  // Insert code here;
+  let count =0;
+  for(let i=0; i<nums.length; i++)
+  {
+    if(nums[i] == i)
+    {
+      count++;
+    }
+  }
+  return count;
 }
+let myNums = [14, 1, 40, 3, 18,];
+console.log(countOfAllIndexMatchingNumbers(myNums));
 
 // Do not edit this line;
 module.exports = countOfAllIndexMatchingNumbers;
