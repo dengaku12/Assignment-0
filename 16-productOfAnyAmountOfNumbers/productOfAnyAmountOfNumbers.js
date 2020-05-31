c247ca5021d70d9343f4afe5448267ff83b6bfad
Michael Odusanya <modusanya12@gmail.com>
2020-05-31 17:16:03 -0400

    multiplies any amount of numbers

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,6 +1,11 @@
 function productOfAnyAmountOfNumbers(...args) {
-  // Insert code here;
+  let prod = 1;
+  for(let i=0; i<args.length; i++)
+  {
+    prod *= args[i];
+  }
+  return prod;
 }
-
+console.log(productOfAnyAmountOfNumbers(1,2,3,4,5));
 // Do not edit this line;
 module.exports = productOfAnyAmountOfNumbers;
