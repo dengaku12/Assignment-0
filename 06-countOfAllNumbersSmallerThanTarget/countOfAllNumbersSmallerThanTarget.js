21c034be9b956e03f0bf3effe0d814bc66e97e62
Michael Odusanya <modusanya12@gmail.com>
2020-05-31 14:26:50 -0400

    counts numbers smaller than target

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,6 +1,16 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
-  // Insert code here;
+  let count = 0;
+  for(let i=0; i<nums.length; i++)
+  {
+    if(nums[i] < target)
+    {
+      count++;
+    }
+  }
+  return count;
 }
+let myNums = [12, 9, 15, 25, 60, 50];
+console.log(countOfAllNumbersSmallerThanTarget(myNums, 20));
 
 // Do not edit this line;
 module.exports = countOfAllNumbersSmallerThanTarget;
