ccaddf69dd7a255186522eed23944ef6d2895db9
Michael Odusanya <modusanya12@gmail.com>
2020-05-31 15:01:01 -0400

    adds the max and min values

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,6 +1,19 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+  let min = nums[0];
+  let max = nums[0];
+  for(let i =1; i<nums.length; i++)
+  {
+    if(nums[i] < min)
+    {
+      min = nums[i];
+    }
+    if(nums[i] > max)
+    {
+      max = nums[i];
+    }
+  }
+  return max + min;
 }
-
+console.log(sumOfMinimumAndMaximum([13,14,10,8,5]));
 // Do not edit this line;
 module.exports = sumOfMinimumAndMaximum;
