2a3883abecb3140c74c2d375cf68af79444598cc
Michael Odusanya <modusanya12@gmail.com>
2020-05-31 16:34:35 -0400

    factorial but addition instead..idk

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,9 +1,18 @@
 class MySolution {
   countDownSum(num) {
-    // Insert code here;
+    if(num <= 1)
+    {
+      return 1;
+    }
+    else
+    {
+      return num + this.countDownSum(num-1);
+    }
+    
   }
 }
-
+let sol = new MySolution();
+console.log(sol.countDownSum(5));
 // Do not edit this line;
 let StudentSolution = MySolution;
 module.exports = StudentSolution;
