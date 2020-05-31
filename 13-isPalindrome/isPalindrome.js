2d786180980be3195c44d7b6bcbd587bb4d9ca18
Michael Odusanya <modusanya12@gmail.com>
2020-05-31 15:27:50 -0400

    checks if word is palindrome

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,6 +1,12 @@
 function isPalindrome(word) {
-  // Insert code here;
+  let reverse = "";
+  for(let i = word.length-1; i>=0; i--)
+  {
+    reverse += word.charAt(i);
+  }
+  return word == reverse;
 }
+console.log(isPalindrome("madam"));
 
 // Do not edit this line;
 module.exports = isPalindrome;
