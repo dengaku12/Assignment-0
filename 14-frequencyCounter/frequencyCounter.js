fee365bb8d59366862d122834143c83abeddcd83
Michael Odusanya <modusanya12@gmail.com>
2020-05-31 16:14:56 -0400

    counts freq of letters in a word

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,6 +1,19 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  let obj = new Object();
+  for(let i =0; i< word.length; i++)
+  {
+    letter = word.charAt(i);
+    if(obj[letter])
+    {
+      obj[letter]++;
+    }
+    else
+    {
+      obj[letter] = 1;
+    }
+  }
+  return obj;
 }
-
+console.log(frequencyCounter("appppppppple"));
 // Do not edit this line;
 module.exports = frequencyCounter;
