8eeff2eb2d2f3be319c0b0e9b601a72a170e385e
Michael Odusanya <modusanya12@gmail.com>
2020-05-31 14:14:05 -0400

    fixed boolean count function

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,6 +1,16 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+  let count =0;
+  for(let i=0; i<arr.length; i++)
+  {
+    if(arr[i]==true || arr[i]==false)
+    {
+      count++;
+    }
+  }
+  return count;
 }
+let myarr = ["Apples", "Corn", true, "crayon",false];
+console.log(countOfAllBooleans(myarr));
 
 // Do not edit this line;
 module.exports = countOfAllBooleans;
