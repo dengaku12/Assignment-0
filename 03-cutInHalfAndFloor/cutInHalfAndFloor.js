9715c851fcc3b49d0fe68b238bfec25f667c5ef8
Michael Odusanya <modusanya12@gmail.com>
2020-05-31 14:07:16 -0400

    fixed cut in hald func

--- 03-cutInHalfAndFloor/cutInHalfAndFloor.js
+++ 03-cutInHalfAndFloor/cutInHalfAndFloor.js
@@ -1,6 +1,13 @@
 function cutInHalfAndFloor(num) {
-  // Insert code here;
+  if(num % 2 == 0)
+  {
+    return num/2;
+  }
+  else
+  {
+    return (num/2) - 0.5;
+  }
 }
-
+console.log(cutInHalfAndFloor(13));
 // Do not edit this line;
 module.exports = cutInHalfAndFloor;
