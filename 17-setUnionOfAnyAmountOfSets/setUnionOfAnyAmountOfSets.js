4b2717a01d4b1eb9de93fb0c486c6e683aa0711a
Michael Odusanya <modusanya12@gmail.com>
2020-05-31 19:53:54 -0400

    produces union set of any amount of sets

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,6 +1,17 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+  let mySet = new Set();
+  for(let i = 0; i<args.length; i++)
+  {
+    for(const elem of args[i])
+    {
+      mySet.add(elem);
+    }
+  }
+  return mySet;
 }
-
+let set1 = new Set([1,3,4,2]);
+let set2 = new Set([10,19,7]);
+let set3 = new Set([24, 30, 8,42]);
+console.log(setUnionOfAnyAmountOfSets(set1,set2,set3));
 // Do not edit this line;
 module.exports = setUnionOfAnyAmountOfSets;
