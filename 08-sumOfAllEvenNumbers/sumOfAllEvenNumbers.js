11ad9b49ab7a9078508a8ca7e68f8e4f0ea8ef00
Michael Odusanya <modusanya12@gmail.com>
2020-05-31 14:38:29 -0400

    returns count of even nums in array

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,6 +1,15 @@
 function sumOfAllEvenNumbers(nums) {
-  // Insert code here;
+  let count = 0;
+  for(let i=0; i<nums.length; i++)
+  {
+    if(nums[i] % 2 ==0)
+    {
+      count++;
+    }
+  }
+  return count;
 }
-
+let myNums = [5, 6, 10, 12, 29, 32];
+console.log(sumOfAllEvenNumbers(myNums));
 // Do not edit this line;
 module.exports = sumOfAllEvenNumbers;
