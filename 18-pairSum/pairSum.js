76186d3bc4ccf89d8635d9d66ba1e6a7d46013fb
Michael Odusanya <modusanya12@gmail.com>
2020-05-31 20:05:26 -0400

    lets you know if 2 nums in an array add up to a chosen target

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,6 +1,18 @@
 function pairSum(nums, target) {
-  // Insert code here;
+  let sumExists = false;
+  for(let i=0; i<nums.length; i++)
+  {
+    for(let j =i+1; j<nums.length; j++)
+    {
+      if(nums[i] + nums[j] == target)
+      {
+        sumExists = true;
+      }
+    }
+  }
+  return sumExists;
 }
-
+let myNums = [1,2,4,5];
+console.log(pairSum(myNums,7));
 // Do not edit this line;
 module.exports = pairSum;
